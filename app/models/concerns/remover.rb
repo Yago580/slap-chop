4849040f7befb4e7863a16f6f6e309fdf28c86b0
Chris Scott <christo247@gmail.com>
2015-10-09 15:02:07 -0700

    Getting ready to push

--- app/models/concerns/remover.rb
+++ app/models/concerns/remover.rb
@@ -3,6 +3,18 @@ module Remover
     get_accepted_content(string.split("\n"), Regexp.new('#.*'))
   end
 
+  def self.uncomment_js(string)
+    get_accepted_content(string.split("\n"), Regexp.new('//.+$'))
+  end
+
+  def self.unlog_rb
+    get_accepted_content(string.split("\n"), Regexp.new('(p |print |put ).+$'))
+  end
+
+  def self.unlog_js
+    get_accepted_content(string.split("\n"), Regexp.new('console\.log.+$'))
+  end
+
   def self.get_accepted_content(array, regexp)
     array.reject {|line| line  =~ regexp}
   end
