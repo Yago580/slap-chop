5d8f8dec466a5ec72295b4550b7cf338c2aaea0c
Chris Scott <christo247@gmail.com>
2015-10-09 21:24:35 -0700

    refactor remover class

--- app/models/concerns/remover.rb
+++ app/models/concerns/remover.rb
@@ -1,21 +1,21 @@
 module Remover
   def self.uncomment_rb(string)
-    string.split("\n").reject {|line| line =~ Regexp.new('#.*')}
+    get_content(string, '#.*')
   end
 
   def self.uncomment_js(string)
-    string.split("\n").reject {|line| line =~ Regexp.new('//.+$')}
+    get_content(string, '//.+$')
   end
 
   def self.unlog_rb(string)
-    string.split("\n").reject {|line| line =~ Regexp.new('(p |print |puts ).*$')}
-end
-
-  def self.unlog_js(string)
-    string.split("\n").reject {|line| line =~ Regexp.new('console\.log.+$')}
+    get_content(string, '(p |print |puts ).*$')
   end
 
-  def self.get_accepted_content(array, regexp)
-    array.reject {|line| line  =~ regexp}
+  def self.unlog_js(string)
+    get_content(string, 'console\.log.+$')
+  end
+
+  def self.get_content(string, regexp)
+    string.split("\n").reject {|line| line =~ Regexp.new(regexp)}
   end
 end
